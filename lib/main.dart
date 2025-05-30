6d520e0746cc519e91124289dccbe3fff6d7119e
dauka123 <rysbek_dauren@mail.ru>
2025-05-30 22:59:30 +0500

    debug removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import flutter_dotenv
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'pages/start_screen.dart';
 import 'pages/home_screen.dart';
@@ -29,6 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.teal,
       ),
+      debugShowCheckedModeBanner: false, // Disable the debug banner
       home: const StartScreen(),
       routes: {
         '/home': (context) => const HomeScreen(),
